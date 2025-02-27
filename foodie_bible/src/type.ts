78007838cb79c6ccb22292353b869f372877bbac
Bambam <bambam.pataranan@gmail.com>
2025-02-27 15:01:55 +0700

    Create 'type.ts' of Food item

---
+++ foodie_bible/src/type.ts
@@ -0,0 +1,20 @@
+export interface Food {
+    RecipeId: number
+    Name: string
+    AuthorName: string
+    CookTime: string
+    PrepTime: string
+    TotalTime: string
+    Description: string
+    Images: string
+    RecipeCategory: string
+    Keywords: string
+    RecipeIngredientQuantities: string
+    RecipeIngredientParts: string
+    AggregatedRating: number
+    ReviewCount: number
+    Calories: number
+    RecipeServings: number
+    RecipeYield: string
+    RecipeInstructions: string
+}
