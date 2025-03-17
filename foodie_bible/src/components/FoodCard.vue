f791a3018974777e5927a1ebe09283218e591e54
Bambam <bambam.pataranan@gmail.com>
2025-03-18 04:23:54 +0700

    Update "FoodCard" can show Images

--- foodie_bible/src/components/FoodCard.vue
+++ foodie_bible/src/components/FoodCard.vue
@@ -6,26 +6,37 @@ defineProps<{
     food: Food
 }>()
 
+
 </script>
 
 <template>
     <RouterLink class="food-link" :to="{ name: 'food-detail-view', params: { id: food.RecipeId }}">
         <div class="food-card">
-            <img :src="food.Images" alt="Food Image" class="food-image" />
+            <img :src="food.image_link" alt="Food Image" class="food-image" />
             <h1>{{ food.Name }}</h1>
+            <div class="category">
+                <p>{{ food.RecipeCategory }}</p>
+            </div>
         </div>
     </RouterLink>
 </template>
 
 
 <style scoped>
-.food-class {
-    padding: 20px;
-    width: 250px;
-    cursor: pointer;
-    border: 1px solid black;
-    margin-bottom: 18px;
+h1 {
+    text-decoration: none;
+    color: #603F26;
     text-align: center;
+}
+
+.food-card {
+    border: solid 5px rgb(187, 4, 4);
+    margin: 10px;
+    padding: 10px;
+    width: 100%; 
+    max-width: 300px; 
+    border-radius: 8px; 
+    background-color: white;
 }
 
 .food-image {
@@ -34,4 +45,23 @@ defineProps<{
     border-radius: 8px;
     margin-bottom: 10px;
 }
+.food-link {
+    text-decoration: none;
+    color: inherit;
+    display: block;
+}
+
+.category {
+    border: solid 2px #FFB200;
+    border-radius: 20px; 
+    padding: 0px 15px; 
+    display: flex; 
+    justify-content: center; 
+    align-items: center; 
+    text-align: center;
+    font-size: 14px;
+    margin: 10px auto; 
+    width: fit-content;
+}
+
 </style>
