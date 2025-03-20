70f837402dc0e3d43ee27b26143c74a500688992
Bambam <bambam.pataranan@gmail.com>
2025-03-21 05:01:50 +0700

    Modify Home View

--- foodie_bible/src/views/HomeView.vue
+++ foodie_bible/src/views/HomeView.vue
@@ -1,37 +1,46 @@
 <script setup lang="ts">
-import FoodCard from '@/components/FoodCard.vue';
-import Food from '@/type/Food'
-import { ref , onMounted } from 'vue'
-import axios from 'axios';
+import { ref, onMounted } from 'vue';
 import FoodService from '@/service/FoodService';
+import FoodCard from '@/components/FoodCard.vue';
+import Food from '@/type/Food';
 
-const foods = ref<Food[]>(null)
+const foods = ref<Food[]>([]);
+const hasRefreshed = localStorage.getItem('hasRefreshed');
 
 onMounted(() => {
-   FoodService.getFoods()
-        .then((response) => {
-            console.log(response.data.results);
-            foods.value = response.data.results;
-        })
-        .catch((error) => {
-            console.error('There was an error!' , error)
-        })
+  if (!hasRefreshed) {
+    localStorage.setItem('hasRefreshed', 'true');
+    window.location.reload();
+  } else {
+    FoodService.getFoods()
+      .then((response) => {
+        foods.value = response.data.results;
+      })
+      .catch((error) => {
+        console.error('There was an error!', error);
+      });
 
-})
-
+    localStorage.removeItem('hasRefreshed');
+  }
+});
 </script>
 
 <template>
-    <div class="foods">
-        <FoodCard v-for="food in foods" :key="food.RecipeId" :food="food" />
-    </div>
+  <div class="foods">
+    <FoodCard v-for="food in foods" :key="food.RecipeId" :food="food" />
+  </div>
 </template>
-
 
 <style scoped>
 .foods {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  column-count: 3;
+  column-gap: 16px;
+  padding: 20px;
+}
+
+.food-card {
+  display: inline-block;
+  width: 100%;
+  margin-bottom: 16px;
 }
 </style>
